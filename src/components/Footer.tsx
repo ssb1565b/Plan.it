de7897e0ca86a1ddb7f653395141c753d9325222
dbswp <dbswp980427@gmail.com>
2023-05-31 15:52:03 +0900

    Feat[#1]: Add test.scss, footer.scss

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/_footer.scss';
 
 export default function Footer() {
   return (
@@ -10,7 +11,7 @@ export default function Footer() {
           </div>
           <div className="footer-text-box">
             <p>우리의 녹색 행성, 더 싱그러워지도록 Plant.it이 함께합니다</p>
-            <p>박성희 ∙ 송민선 ∙ 송수빈 ∙ 윤제</p>
+            <p> 송민선 ∙ 송수빈 ∙ 윤제</p>
             <p>© copyright 2023 plant.it</p>
           </div>
         </div>
--- src/components/TestQuiz.tsx
+++ src/components/TestQuiz.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import { Link } from 'react-router-dom';
 import Footer from './Footer';
+import '../styles/test.scss';
 
 export default function TestQuiz() {
   return (
